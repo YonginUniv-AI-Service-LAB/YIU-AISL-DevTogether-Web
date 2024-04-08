f502b63e9eeadf884ce0a57657a1fc98c5de8f5e
min woo <zomin9546@naver.com>
2024-04-08 13:23:35 +0900

    Merge branch 'main' of https://github.com/YonginUniv-AI-Service-LAB/YIU-AISL-DevTogether-Web into minwooyo

--- src/components/Modal/FilterModal.js
+++ src/components/Modal/FilterModal.js
@@ -5,9 +5,10 @@ const FilterModal = ({ isOpen, closeModal }) => {
       // 페이지 이동
     return (
         <div className={style.filter} style={{ display: isOpen ? 'block' : 'none' }}>
-            <h1>필터를 키면 보입니다</h1>
-            <div style={{ color: 'red' }}>필터를 키면 보입니다필터를 키면 보입니다필터를 키면 보입니다필터를 키면 보입니다필터를 키면 보입니다필터를 키면 보입니다</div>
+            <div>
+            <div> </div>
             <button onClick={closeModal}>모달 닫기</button>
+            </div>
         </div>
     );
 };
