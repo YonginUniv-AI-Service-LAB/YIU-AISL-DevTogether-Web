aec19a670112e8ecb3ab4e26694f057bfd486635
Meehyun Byeon <bmh2038@naver.com>
2024-05-03 09:30:11 +0900

    feature: create FAQ Form UI

--- src/pages/FAQForm/FAQForm.js
+++ src/pages/FAQForm/FAQForm.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import { useNavigate } from "react-router-dom";
-
+import PageHeader from "../../components/Group/PageHeader/PageHeader";
+import TextArea from "antd/es/input/TextArea";
+import FormLabelText from "../../components/Text/FormLabel";
+import { Input, Form } from "antd";
+import DefaultButton from "../../components/Button/DefaultButton";
 const FAQFormPage = () => {
   // 반응형 화면
   const isDesktopOrLaptop = useMediaQuery({ minWidth: 992 });
@@ -12,9 +16,76 @@ const FAQFormPage = () => {
   // 페이지 이동
   const navigate = useNavigate();
 
+  const [value, setValue] = useState("");
+
   return (
     <div>
-      <h1>FAQ 폼 - 작성, 수정</h1>
+      <PageHeader
+        title="FAQ 작성"
+        // subtitle="DevTogether의 다양한 소식을 알려드립니다."
+      />
+      <Form
+        name="wrap"
+        labelCol={{
+          flex: "120px",
+        }}
+        labelAlign="left"
+        labelWrap
+        wrapperCol={{
+          flex: 1,
+        }}
+        colon={false}
+        style={{
+          margin: 300,
+          marginTop: 80,
+        }}
+      >
+        <Form.Item
+          label={<FormLabelText text="질문" />}
+          name="contents"
+          style={{ marginBottom: 50 }}
+        >
+          <TextArea
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="질문 입력"
+            autoSize={{
+              minRows: 5,
+              // maxRows: 100,
+            }}
+            // count={{
+            //   show: true,
+            //   max: 3000,
+            // }}
+            size={"large"}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label={<FormLabelText text="답변" />}
+          name="contents"
+          style={{ marginBottom: 50 }}
+        >
+          <TextArea
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="답변 입력"
+            autoSize={{
+              minRows: 15,
+              // maxRows: 100,
+            }}
+            // count={{
+            //   show: true,
+            //   max: 3000,
+            // }}
+            size={"large"}
+          />
+        </Form.Item>
+
+        <Form.Item label=" ">
+          <DefaultButton text="게시" />
+        </Form.Item>
+      </Form>
     </div>
   );
 };
