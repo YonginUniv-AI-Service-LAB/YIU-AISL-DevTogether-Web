78187b6d168116c2b0dc38292cf11504fc70874f
Meehyun Byeon <bmh2038@naver.com>
2024-05-29 10:29:41 +0900

    refactor: FAQ 작성/수정 상태관리

--- src/pages/FAQForm/FAQForm.js
+++ src/pages/FAQForm/FAQForm.js
@@ -6,6 +6,8 @@ import TextArea from "antd/es/input/TextArea";
 import FormLabelText from "../../components/Text/FormLabel";
 import { Input, Form } from "antd";
 import DefaultButton from "../../components/Button/DefaultButton";
+import { FAQFormDataAtom, FAQFormTypeAtom } from "../../recoil/atoms/faq";
+import { useRecoilState, useRecoilValue } from "recoil";
 const FAQFormPage = () => {
   // 반응형 화면
   const isDesktopOrLaptop = useMediaQuery({ minWidth: 992 });
@@ -18,10 +20,54 @@ const FAQFormPage = () => {
 
   const [value, setValue] = useState("");
 
+  // 폼 타입 => 작성 || 수정
+  const formType = useRecoilValue(FAQFormTypeAtom);
+  // 폼 데이터 세팅
+  const [formData, setFormData] = useRecoilState(FAQFormDataAtom);
+
+  // 데이터
+  const [form, setForm] = useState({
+    faqId: {
+      value: formType === "update" ? formData.faqId : 0,
+      type: "textInput",
+      // rules: {
+      //   isRequired: true,
+      // },
+      valid: false,
+    },
+    title: {
+      value: formType === "update" ? formData.title : "",
+      type: "textInput",
+      rules: {
+        isRequired: true,
+      },
+      valid: false,
+    },
+    contents: {
+      value: formType === "update" ? formData.contents : "",
+      type: "textInput",
+      rules: {
+        isRequired: true,
+      },
+      valid: false,
+    },
+  });
+
+  // 텍스트인풋 업데이트
+  const onChange = (e) => {
+    setForm((prevState) => ({
+      ...prevState,
+      [e.target.id]: {
+        ...prevState[e.target.id],
+        value: e.target.value,
+      },
+    }));
+  };
+
   return (
     <div>
       <PageHeader
-        title="FAQ 작성"
+        title={formType == "create" ? "FAQ 작성" : "FAQ 수정"}
         // subtitle="DevTogether의 다양한 소식을 알려드립니다."
       />
       <Form
@@ -42,12 +88,14 @@ const FAQFormPage = () => {
       >
         <Form.Item
           label={<FormLabelText text="질문" />}
-          name="contents"
+          name="title"
           style={{ marginBottom: 50 }}
         >
           <TextArea
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            id={"title"}
+            value={form.title.value}
+            defaultValue={form.title.value}
+            onChange={onChange}
             placeholder="질문 입력"
             autoSize={{
               minRows: 5,
@@ -67,8 +115,10 @@ const FAQFormPage = () => {
           style={{ marginBottom: 50 }}
         >
           <TextArea
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+            id={"contents"}
+            value={form.contents.value}
+            defaultValue={form.contents.value}
+            onChange={onChange}
             placeholder="답변 입력"
             autoSize={{
               minRows: 15,
--- src/pages/FAQList/FAQList.js
+++ src/pages/FAQList/FAQList.js
@@ -1,11 +1,12 @@
-import { Collapse, Spin, theme } from "antd";
+import { Button, Collapse, Popconfirm, Spin, theme } from "antd";
 import React from "react";
 import { useMediaQuery } from "react-responsive";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { data_faq } from "../../assets/data/faq";
 import PageHeader from "../../components/Group/PageHeader/PageHeader";
 import { colors } from "../../assets/colors";
 import Accordion from "react-bootstrap/Accordion";
+import { message } from "antd";
 import { useAccordionButton } from "react-bootstrap/AccordionButton";
 import Card from "react-bootstrap/Card";
 import styles from "./FAQList.module.css";
@@ -13,8 +14,16 @@ import PageHeaderImage from "../../assets/images/PageHeaderImage/faq.svg";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 import { defaultAPI } from "../../api";
+import HoverEventButton from "../../components/Button/HoverEventButton";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { FAQFormDataAtom, FAQFormTypeAtom } from "../../recoil/atoms/faq";
+import {
+  DeleteOutlined,
+  EditOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
 
-const FAQListPage = () => {
+const FAQListPage = (props) => {
   // 반응형 화면
   const isDesktopOrLaptop = useMediaQuery({ minWidth: 992 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 991 });
@@ -33,12 +42,46 @@ const FAQListPage = () => {
     },
   });
 
+  // 임시
+  const manager = true;
+
   // 페이지 이동
   const navigate = useNavigate();
+  // 이전 페이지에서 데이터 가져오기
+  const location = useLocation();
+
+  // 폼 타입 => 작성
+  const setFormType = useSetRecoilState(FAQFormTypeAtom);
+
+  const updateConfirm = (e) => {
+    // console.log("FAQ 폼데이터", e);
+    // message.success("Click on Yes");
+    setFormType("update");
+    setFormData(e);
+    navigate("/faq/form");
+  };
+  const updateCancel = (e) => {
+    console.log(e);
+    // message.error("Click on No");
+  };
+
+  const deleteConfirm = (e) => {
+    console.log(e);
+    message.success("FAQ 삭제 완료");
+    // navigate(-1);
+  };
+  const deleteCancel = (e) => {
+    console.log(e);
+    // message.error("Click on No");
+  };
+
+  // 폼 데이터 세팅
+  const [formData, setFormData] = useRecoilState(FAQFormDataAtom);
 
   function CustomToggle({ children, eventKey }) {
     const decoratedOnClick = useAccordionButton(eventKey, () =>
-      console.log("totally custom!")
+      // setFormData(eventKey)
+      console.log(children, eventKey)
     );
 
     return (
@@ -65,6 +108,22 @@ const FAQListPage = () => {
     <div>
       <PageHeader title="자주묻는질문" subtitle="" image={PageHeaderImage} />
       <div style={{ padding: isMobile ? "10%" : "15%" }}>
+        {manager ? (
+          <div style={{ textAlign: "end", marginBottom: 30 }}>
+            <HoverEventButton
+              title={"FAQ 작성"}
+              onClick={() => {
+                setFormType("create");
+                navigate("/faq/form");
+              }}
+              size={"middle"}
+              bgColor={colors.sub}
+              bgColor_hover={colors.main}
+              fontColor={"white"}
+              fontColor_hover={"white"}
+            />
+          </div>
+        ) : null}
         <Accordion defaultActiveKey="0" flush={true}>
           {faq.map((data, index) => {
             return (
@@ -77,19 +136,51 @@ const FAQListPage = () => {
                       display: "flex",
                       alignItems: "center",
                       fontSize: 20,
+                      justifyContent: "space-between",
                     }}
                   >
-                    <span
-                      style={{
-                        color: colors.main,
-                        fontWeight: "bold",
-                        fontSize: 30,
-                        marginRight: 10,
-                      }}
-                    >
-                      Q{" "}
+                    <span style={{ display: "flex", alignItems: "center" }}>
+                      <span
+                        style={{
+                          color: colors.main,
+                          fontWeight: "bold",
+                          fontSize: 30,
+                          marginRight: 10,
+                        }}
+                      >
+                        Q{" "}
+                      </span>
+                      {data.title}
                     </span>
-                    {data.title}
+                    <span>
+                      <Popconfirm
+                        title="FAQ 수정"
+                        description="FAQ를 수정하시겠습니까?"
+                        onConfirm={() => updateConfirm(data)}
+                        onCancel={updateCancel}
+                        okText="수정"
+                        cancelText="취소"
+                      >
+                        <Button type="text" icon={<EditOutlined />} />
+                      </Popconfirm>
+                      <Popconfirm
+                        title="공지사항 삭제"
+                        description="FAQ를 삭제하시겠습니까?"
+                        onConfirm={deleteConfirm}
+                        onCancel={deleteCancel}
+                        okText="삭제"
+                        cancelText="취소"
+                        icon={
+                          <ExclamationCircleOutlined
+                            style={{
+                              color: "red",
+                            }}
+                          />
+                        }
+                      >
+                        <Button type="text" icon={<DeleteOutlined />} />
+                      </Popconfirm>
+                    </span>
                   </span>
                 </CustomToggle>
                 <Accordion.Collapse
